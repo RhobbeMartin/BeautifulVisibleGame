af6c4397cfd19316366d95827ee8762737a22f48
Martin Rhobbe <118451@eesd.org>
2021-01-11 23:57:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print ("Hello World")# This print statement prints out 'Hello World'
+
+# Write 5 print statements
+# Name, Your Grade, Favorite color, Favorite sport, favorite ice cream flavor
+
+print ("Rhobbe Martin")
+
+print ("7th grade")
+
+print ("Red")
+
+print ("BasketBall")
+
+print ("Cookies and Cream")
